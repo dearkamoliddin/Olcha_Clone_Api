c5cdcecf7e93fe888be73276bf3890a36d10b920
dearkamoliddin <dearkamoliddin@gmail.com>
2024-08-06 15:08:47 +0500

    generic class-based views

--- olcha/urls.py
+++ olcha/urls.py
@@ -2,12 +2,6 @@ from django.urls import path
 from olcha import views
 
 urlpatterns = [
-    # path('category/', views.CategoryListApiView.as_view(), name='category'),
-    # path('category/create/', views.CategoryListApiView.as_view(), name='category-create'),
-    #
-    # path('category/<slug:slug>/detail/', views.CategoryDetailApiView.as_view(), name='category-detail'),
-    # path('category/<slug:slug>/edit/', views.CategoryDetailApiView.as_view(), name='category-update'),
-    # path('category/<slug:slug>/delete/', views.CategoryDetailApiView.as_view(), name='category-delete'),
-    path('category/', views.CategoryList.as_view()),
-    path('category/<int:pk>/edit/', views.CategoryDetail.as_view()),
+    path('category/', views.CategoryList.as_view(), name='category-list'),
+    path('category/<int:pk>/edit/', views.CategoryDetail.as_view(), name='category-detail'),
 ]
--- olcha/views.py
+++ olcha/views.py
@@ -1,11 +1,3 @@
-# from rest_framework import status
-# from rest_framework.permissions import AllowAny
-# from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
-# from rest_framework.response import Response
-# from rest_framework.views import APIView
-# from olcha.serializers import CategoryModelSerializer
-# from olcha.models import CategoryModel
-
 from olcha.models import CategoryModel
 from olcha.serializers import CategoryModelSerializer
 from rest_framework import generics
@@ -19,48 +11,4 @@ class CategoryList(generics.ListCreateAPIView):
 class CategoryDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = CategoryModel.objects.all()
     serializer_class = CategoryModelSerializer
-
-
-# class CategoryListApiView(APIView):
-#     def get(self, request):
-#         categories = CategoryModel.objects.all()
-#         serializer = CategoryModelSerializer(categories, many=True)
-#         return Response(serializer.data, status=status.HTTP_200_OK)
-#
-#     def post(self, request):
-#         serializer = CategoryModelSerializer(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response('Category successfully created!', status=status.HTTP_201_CREATED)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-#
-#
-# class CategoryDetailApiView(APIView):
-#     def get(self, request, slug):
-#         category = CategoryModel.objects.get(slug=slug)
-#         serializer = CategoryModelSerializer(category)
-#         return Response(serializer.data, status=status.HTTP_200_OK)
-#
-#     def put(self, request, slug):
-#         category = CategoryModel.objects.get(slug=slug)
-#         serializer = CategoryModelSerializer(data=request.data, instance=category)
-#         if serializer.is_valid():
-#             serializer.save()
-#             data = {
-#                 'message': 'Category successfully updated!',
-#                 'status': status.HTTP_200_OK,
-#             }
-#             return Response(data)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-#
-#     def delete(self, request, slug):
-#         category = CategoryModel.objects.get(slug=slug)
-#         if category:
-#             category.delete()
-#             data = {
-#                 'message': 'Category successfully deleted!',
-#                 'status': status.HTTP_200_OK,
-#             }
-#             return Response(data)
-
 
